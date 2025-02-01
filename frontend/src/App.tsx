68bdc6f1d7f81b7f13e7dd675f2b6688a823da7f
Aditya Singh <yeezusgod01@gmail.com>
2025-02-01 23:06:28 +0530

    Added some optimizations in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,14 +4,22 @@ import Login from "./pages/Login";
 import SignUp from "./pages/Signup";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { useState } from "react";
+import RefreshHandler from "./refreshHandler";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const PrivateRoute = ({ element }: any) => {
+    return isAuthenticated ? element : <Navigate to="/login" />;
+  };
+
   return (
-    <div>
+    <div className="App">
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
